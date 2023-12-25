4a0be9edac4465f412e6e178e7cfb582789be3d4
adjet-man <richadjetey@gmail.com>
2023-12-25 16:02:22 +0000

    Book Review

---
+++ router/booksdb.js
@@ -0,0 +1,14 @@
+let books = {
+      1: {"author": "Harper Lee","title": "To Kill a Mockingbird", "reviews": {} },
+      2: {"author": "George Orwell","title": "1984", "reviews": {} },
+      3: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
+      4: {"author": "J.K. Rowling","title": "Harry Potter and the Sorcerer's Stone", "reviews": {} },
+      5: {"author": "J.D. Salinger","title": "The Catcher in the Rye", "reviews": {} },
+      6: {"author": "J. R. R. Tolkien","title": "The Lord of the Rings", "reviews": {} },
+      7: {"author": "Leo Tolstoy","title": "One Hundred Years of Solitude", "reviews": {} },
+      8: {"author": "Douglas Adams","title": "The Hitchhiker's Guide to the Galaxy", "reviews": {} },
+      9: {"author": "Dan Brown","title": "The Da Vinci Code", "reviews": {} },
+      10: {"author": "F. Scott Fitzgerald","title": "The Great Gatsby", "reviews": {} }
+}
+
+module.exports=books;
